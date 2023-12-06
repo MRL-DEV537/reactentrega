0392ca64594c4a748f07891a8c919ba56a1d61f4
León Santa Maria <leonsantamariacontact@gmail.com>
2023-12-05 22:07:49 -0300

    PreEntrega+Santa Maria

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import NavScrollExample from './components/NavBar.js';
+
+
+function App() {
+  return (
+    <div>
+      <NavScrollExample/>
+      <ul>
+        <a href="#"><li>Computadoras Armadas</li></a>
+        <a href="#"><li>CPU</li></a>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
